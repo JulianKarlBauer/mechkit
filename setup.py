3e22aece5d3501d2e474cb1443e8b7ccee4f1ffd
Julian Bauer KIT <julian.bauer@kit.edu>
2019-07-08 16:58:41 +0200

    Add setup, upload on Pypi

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mechkit",
+    version="0.0.1",
+    author="Julian Karl Bauer",
+    author_email="JulianBauerKIT@gmail.com",
+    description="Basic tools for continuum mechanics using numpy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JulianBauerKIT/mechkit",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'numpy',
+          ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
