a113298d404bc1a6a00767bc9b8c883502c1151e
JulianKarlBauer <JulianKarlBauer@gmx.de>
2019-11-20 13:12:30 +0100

    Release 0.2.0
    Remember: 0.1.211 is larger than 0.1.22

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="mechkit",
-    version="0.1.211",
+    version="0.2.0",
     author="Julian Karl Bauer",
     author_email="JulianKarlBauer@gmx.de",
     description="Basic continuum mechanics toolkit",
