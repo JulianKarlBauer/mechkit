09db49da19df1a287eb0e97ddcc3c7508d756317
JulianKarlBauer <JulianKarlBauer@gmx.de>
2020-04-29 21:05:26 +0200

    Final thought on valid combinations

--- s010.py
+++ s010.py
@@ -1,10 +1,26 @@
 import itertools
+from pprint import pprint
 
-fixed = ["G12"]
-variable = ['E1', 'E2', "G23", "nu12", "nu21", "nu23"]
-variable_combinations = list(itertools.combinations(variable, 4))
-combinations = [fixed + list(comb) for comb in variable_combinations]
+items = ["E1", "E2", "G12", "G23", "nu12", "nu21", "nu23"]
 
-# Notes: G12 is required!
+combinations = list(itertools.combinations(items, 5))
 
+# Directly required
+contain_E1_E2_G12 = [
+    comb for comb in combinations if ("E1" in comb and "E2" in comb and "G12" in comb)
+]
 
+# Two possibilities to imply G23
+contain_E1_E2_G12_imply_G23 = [
+    comb for comb in contain_E1_E2_G12 if ("G23" in comb or "nu23" in comb)
+]
+
+# Two possibilities to imply nu12
+contain_E1_E2_G12_imply_G23_nu12 = [
+    comb for comb in contain_E1_E2_G12_imply_G23 if ("nu12" in comb or "nu21" in comb)
+]
+
+possible_input = contain_E1_E2_G12_imply_G23_nu12
+
+print('Possible input combinations are')
+pprint(possible_input)
