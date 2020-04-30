cb67c18e103698ba2507b214db074870c96da45c
JulianKarlBauer <JulianKarlBauer@gmx.de>
2020-04-30 16:13:01 +0200

    Black this file

--- test/test_material.py
+++ test/test_material.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-'''Run tests:
+"""Run tests:
     python3 -m pytest
-'''
+"""
 
 import sys
 import os
@@ -11,48 +11,48 @@ import itertools
 import copy
 import pytest
 
-sys.path.append(os.path.join('..'))
+sys.path.append(os.path.join(".."))
 import mechkit
 
 np.set_printoptions(
-        linewidth=140,
-        precision=2,
-        )
+    linewidth=140, precision=2,
+)
+
+##############################################################################
+# Isotropic
 
 
 def steel_scalars():
     return {
-        'E':    2e5,
-        'nu':   0.3,
-        'K':    5e5/3.,
-        'la':   1.5e6/13,
-        'G':    1e6/13,
-        'M':    3.5e6/13,
-        }
+        "E": 2e5,
+        "nu": 0.3,
+        "K": 5e5 / 3.0,
+        "la": 1.5e6 / 13,
+        "G": 1e6 / 13,
+        "M": 3.5e6 / 13,
+    }
 
 
 def add_stiffnesses(inp):
     con = mechkit.notation.VoigtConverter(silent=True)
     tensors = mechkit.tensors.Basic()
 
-    inp['stiffness'] = 3.*inp['K']*tensors.P1 + 2.*inp['G']*tensors.P2
-    inp['stiffness_mandel6'] = con.to_mandel6(inp['stiffness'])
-    inp['stiffness_voigt'] = con.mandel6_to_voigt(
-                                    inp['stiffness_mandel6'],
-                                    voigt_type='stiffness',
-                                    )
+    inp["stiffness"] = 3.0 * inp["K"] * tensors.P1 + 2.0 * inp["G"] * tensors.P2
+    inp["stiffness_mandel6"] = con.to_mandel6(inp["stiffness"])
+    inp["stiffness_voigt"] = con.mandel6_to_voigt(
+        inp["stiffness_mandel6"], voigt_type="stiffness",
+    )
     return inp
 
 
 def add_compliances(inp):
     con = mechkit.notation.VoigtConverter(silent=True)
 
-    inp['compliance_mandel6'] = np.linalg.inv(inp['stiffness_mandel6'])
-    inp['compliance'] = con.to_tensor(inp['compliance_mandel6'])
-    inp['compliance_voigt'] = con.mandel6_to_voigt(
-                                    inp['compliance_mandel6'],
-                                    voigt_type='compliance',
-                                    )
+    inp["compliance_mandel6"] = np.linalg.inv(inp["stiffness_mandel6"])
+    inp["compliance"] = con.to_tensor(inp["compliance_mandel6"])
+    inp["compliance_voigt"] = con.mandel6_to_voigt(
+        inp["compliance_mandel6"], voigt_type="compliance",
+    )
 
 
 def add_tensors(inp):
@@ -64,12 +64,12 @@ def add_tensors(inp):
 
 def add_additive_tensors(inp):
     out = copy.deepcopy(inp)
-    out.pop('nu')
+    out.pop("nu")
     add_stiffnesses(out)
     return out
 
 
-@pytest.mark.filterwarnings('ignore::UserWarning')
+@pytest.mark.filterwarnings("ignore::UserWarning")
 def test_variants_arguments_steel():
     steel = steel_scalars()
     steel_tensor = add_tensors(inp=steel)
@@ -81,37 +81,32 @@ def test_variants_arguments_steel():
 
 def test_reference():
     return {
-            'C11_voigt': 15.755,                  # e11 Pa
-            'C44_voigt': 5.3184,                  # e11 Pa
-            'C12_voigt': 5.11850,                 # e11 Pa
-            'nu': 0.2452,
-            'E': 13.245,                    # e11 Pa
-            }
+        "C11_voigt": 15.755,  # e11 Pa
+        "C44_voigt": 5.3184,  # e11 Pa
+        "C12_voigt": 5.11850,  # e11 Pa
+        "nu": 0.2452,
+        "E": 13.245,  # e11 Pa
+    }
 
 
-@pytest.mark.filterwarnings('ignore::UserWarning')
+@pytest.mark.filterwarnings("ignore::UserWarning")
 def test_reference_values():
     inp = test_reference()
     for comb in itertools.combinations(inp.keys(), 2):
-        print('######################\n', comb)
+        print("######################\n", comb)
         mat = mechkit.material.Isotropic(**{k: inp[k] for k in comb})
-        keys = ['E', 'nu']
+        keys = ["E", "nu"]
         for key in keys:
             calculated = getattr(mat, key)
             reference = inp[key]
             print(key)
-            print('calculated: ', calculated)
-            print('reference: ', reference)
+            print("calculated: ", calculated)
+            print("reference: ", reference)
             print()
-            assert np.allclose(
-                        calculated,
-                        reference,
-                        rtol=1e-4,
-                        atol=1e-3,
-                        )
+            assert np.allclose(calculated, reference, rtol=1e-4, atol=1e-3,)
 
 
-@pytest.mark.filterwarnings('ignore::UserWarning')
+@pytest.mark.filterwarnings("ignore::UserWarning")
 def test_access_dict_like():
     steel = steel_scalars()
     steel_tensor = add_tensors(inp=steel)
@@ -121,7 +116,7 @@ def test_access_dict_like():
             assert np.allclose(mat[key], val)
 
 
-@pytest.mark.filterwarnings('ignore::UserWarning')
+@pytest.mark.filterwarnings("ignore::UserWarning")
 def test_use_aliases():
     # Get aliases
     mat = mechkit.material.Isotropic(E=1, nu=0.3)
@@ -134,11 +129,8 @@ def test_use_aliases():
         for alias0 in aliases[comb[0]]:
             for alias1 in aliases[comb[1]]:
                 mat = mechkit.material.Isotropic(
-                                **{
-                                    alias0:     steel[comb[0]],
-                                    alias1:     steel[comb[1]],
-                                    }
-                                )
+                    **{alias0: steel[comb[0]], alias1: steel[comb[1]],}
+                )
                 for key, val in steel_tensor.items():
                     assert np.allclose(getattr(mat, key), val)
 
@@ -149,15 +141,15 @@ def test_arithmetic_add():
     result = add_tensors(inp)
     additive_result = add_additive_tensors(inp)
 
-    comb = ['K', 'G']
+    comb = ["K", "G"]
     mat0 = mechkit.material.Isotropic(**{k: additive_result[k] for k in comb})
     mat = mat0 + mat0
 
     for key, val in additive_result.items():
-        assert np.allclose(mat[key], 2*val)
+        assert np.allclose(mat[key], 2 * val)
 
-    for key in ['compliance_mandel6', 'compliance']:
-        assert np.allclose(mat[key], 0.5*result[key])
+    for key in ["compliance_mandel6", "compliance"]:
+        assert np.allclose(mat[key], 0.5 * result[key])
 
 
 def test_arithmetic_mult_sub():
@@ -165,12 +157,12 @@ def test_arithmetic_mult_sub():
     inp = steel_scalars()
     additive_result = add_additive_tensors(inp)
 
-    comb = ['K', 'G']
+    comb = ["K", "G"]
     mat0 = mechkit.material.Isotropic(**{k: additive_result[k] for k in comb})
-    mat = 4*mat0 - mat0
+    mat = 4 * mat0 - mat0
 
     for key, val in additive_result.items():
-        assert np.allclose(mat[key], 3*val)
+        assert np.allclose(mat[key], 3 * val)
 
 
 def test_exception_nbr_parameter():
@@ -185,9 +177,11 @@ def test_exception_duplicate_parameter():
     assert "Redundant" in str(excinfo.value)
 
 
-if __name__ == '__main__':
+##############################################################################
+# Transversal-Isotropic
+
+
+
+
+if __name__ == "__main__":
     test_reference_values()
-
-
-
-
