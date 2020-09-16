0e20e68f8357025946e1cdae7ab9af50be174b61
JulianKarlBauer <JulianKarlBauer@gmx.de>
2020-09-16 11:54:30 +0200

    Play in main.py

--- main.py
+++ main.py
@@ -13,10 +13,21 @@ con = mechkit.notation.AbaqusConverter(silent=True)
 a = TensorComponents(np.arange(9).reshape(3, 3))
 
 tensor = TensorComponents(
-    np.arange(9).reshape(3, 3), quantity="stress", notation="tensor"
+    np.arange(9, dtype=np.float64).reshape(3, 3), quantity="stress", notation="tensor"
 )
+
+assert np.allclose(tensor, tensor.to_mandel9().to_tensor())
+
 mandel6 = TensorComponents(np.arange(6), quantity="stress", notation="mandel6")
 mandel9 = TensorComponents(np.arange(9), quantity="stress", notation="mandel9")
+
+# Vectorized
+stiff_tensor = TensorComponents(
+    np.arange(324, dtype=np.float64).reshape(4, 3, 3, 3, 3),
+    quantity="stiffness",
+    notation="tensor",
+)
+stiff_mandel6 = stiff_tensor.to_mandel6()
 
 
 # mandel = np.array([1., 2, 3, 4, 5, 6])
