41ef8a9abfc5990deaac34ce15cea224603a8583
JulianKarlBauer <juliankarlbauer@gmx.de>
2022-01-03 19:06:07 +0100

    Use README on PyPI

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import setuptools
+from os import path
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="mechkit",
@@ -10,8 +12,8 @@ setuptools.setup(
     author_email="JulianKarlBauer@gmx.de",
     description="Basic continuum mechanics toolkit",
     long_description="Basic continuum mechanics toolkit",
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/JulianKarlBauer/mechkit",
     packages=setuptools.find_packages(),
     install_requires=[
