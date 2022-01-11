84462c1b15756472409dc94edfb59625566b4e89
JulianKarlBauer <juliankarlbauer@gmx.de>
2022-01-11 14:38:38 +0100

    Swicth order: on top, usual case

--- mechkit/__init__.py
+++ mechkit/__init__.py
@@ -1,19 +1,19 @@
 import sys
 
-if sys.version_info > (3, 0):
-    from . import notation
-    from . import tensors
-    from . import utils
-    from . import material
-    from . import fabric_tensors
-    from . import visualization
-else:
+if sys.version_info < (3, 0):
     import notation
     import tensors
     import utils
     import material
     import fabric_tensors
     import visualization
+else:
+    from . import notation
+    from . import tensors
+    from . import utils
+    from . import material
+    from . import fabric_tensors
+    from . import visualization
 
 # import pkg_resources
 #
