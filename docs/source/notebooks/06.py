da6b1f182424201a79af0ad0ea5a239e034d5887
Jonas Hund <jonas.hund@ntnu.no>
2022-02-16 12:33:51 +0100

    old version graphs for notebooks

--- docs/source/notebooks/06.py
+++ docs/source/notebooks/06.py
@@ -1,35 +1,25 @@
 # # Notation: Draw Supported Notations of Explicit Converter
 
 import mechkit
+import networkx as nx
 import matplotlib.pyplot as plt
-import netgraph
 
 plot_options = dict(
-    edge_width=2,
+    node_color="yellow",
+    node_size=2000,
+    width=2,
     arrows=True,
-    # node_shape="h",
-    node_size=10,
-    node_color="#E69F00",
-    node_edge_width=0,
-    node_labels=True,
-    node_label_fontdict=dict(size=30),
+    font_size=10,
+    font_color="black",
 )
 
-pos = {
-    "mandel6": (0.33, 0.5),
-    "mandel9": (0.15, 0.25),
-    "tensor": (0.15, 0.75),
-    "voigt": (0.67, 0.5),
-    "umat": (0.85, 0.25),
-    "vumat": (0.85, 0.75),
-    "abaqusMaterialAnisotropic": (1.0, 0.5),
-}
 converter = mechkit.notation.ExplicitConverter()
 
 for entity_type, graph in converter.graphs_dict.items():
-    fig = plt.figure(figsize=(15, 15))
-    g = netgraph.Graph(graph, node_layout=pos, **plot_options)
-    if entity_type == "stiffness":
-        g.node_label_artists["abaqusMaterialAnisotropic"].set_size(13),
+
+    pos = nx.spring_layout(graph, seed=1)
+
+    fig = plt.figure()
+    nx.draw_networkx(graph, **plot_options)
     plt.gca().set_title(entity_type)
     plt.tight_layout()
